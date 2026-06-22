75cb4d1b481f0cc5ae0ebba929b9e8ed415399f5
barbaraalencard <barbaraalencard@gmail.com>
2026-06-22 18:57:35 -0300

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -141,3 +141,73 @@ window.carregarReceitasFirebase = async function(){
     return [];
 
 };
+
+// ==========================
+// SENHA
+// ==========================
+
+window.salvarSenhaFirebase = async function(senha){
+
+    await setDoc(
+        doc(db, "usuarios", "babi"),
+        {
+            senha: senha
+        },
+        {
+            merge: true
+        }
+    );
+
+};
+
+window.carregarSenhaFirebase = async function(){
+
+    const documento =
+        await getDoc(
+            doc(db, "usuarios", "babi")
+        );
+
+    if(documento.exists()){
+
+        return documento.data().senha || "Flora2026";
+
+    }
+
+    return "Flora2026";
+
+};
+
+// ==========================
+// SENHA
+// ==========================
+
+window.salvarSenhaFirebase = async function(senha){
+
+    await setDoc(
+        doc(db, "usuarios", "babi"),
+        {
+            senha: senha
+        },
+        {
+            merge: true
+        }
+    );
+
+};
+
+window.carregarSenhaFirebase = async function(){
+
+    const documento =
+        await getDoc(
+            doc(db, "usuarios", "babi")
+        );
+
+    if(documento.exists()){
+
+        return documento.data().senha || "Flora2026";
+
+    }
+
+    return "Flora2026";
+
+};
